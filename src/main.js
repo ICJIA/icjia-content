6d7f13a310f28033abdad22d2b349ab12e206a60
cschweda <cschweda@gmail.com>
2022-08-18 10:10:39 -0500

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,17 @@
+import "regenerator-runtime/runtime";
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import vuetify from "./plugins/vuetify";
+import { createProvider } from "./vue-apollo";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  vuetify,
+  apolloProvider: createProvider(),
   render: (h) => h(App),
 }).$mount("#app");
